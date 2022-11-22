960b47fff18fb308d4b0ac33e5e590e1398df7f6
TheWyller <wyller_gt@hotmail.com>
2022-11-22 15:42:42 -0300

    FIX: Ajustando lógica de manipulação de arquivos

--- home/serializers.py
+++ home/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Home
 from utils.converter import converter_CNAB, read_CNAB_json
 from interpreters.serializers import InterpreterSerializer
+import os
 
 
 class HomeSerializer(serializers.ModelSerializer):
@@ -10,7 +11,9 @@ class HomeSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
     def create(self, validated_data):
-        txt_info, _ = Home.objects.get_or_create(validated_data)
+        if os.path.isfile('infos/CNAB.txt'):
+            os.remove('infos/CNAB.txt')
+        txt_info = Home.objects.create(**validated_data)
         converter_CNAB()
         list_info = read_CNAB_json()
         for info in list_info:
