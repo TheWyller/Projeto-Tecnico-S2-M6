2143618a8b046871c19f26cedfa53c585fbf77dc
TheWyller <wyller_gt@hotmail.com>
2022-11-23 10:33:50 -0300

    FIX: Ajustando filtro

--- interpreters/views.py
+++ interpreters/views.py
@@ -5,8 +5,18 @@ from .models import Interpreter
 from django_filters import rest_framework as filters
 
 
+class InterpreterFilter(filters.FilterSet):
+    store_name = filters.CharFilter(
+        field_name="store_name", lookup_expr="icontains"
+    )
+
+    class Meta:
+        model = Interpreter
+        fields = ["store_name"]
+
+
 class InterpreterView(generics.ListCreateAPIView):
     serializer_class = InterpreterSerializer
     queryset = Interpreter.objects.all()
     filter_backends = (filters.DjangoFilterBackend,)
-    filterset_fields = ("store_name",)
+    filterset_class = InterpreterFilter
